334d11061638d30bc7ce5b5b5283ea43e57ad263
Hengsheng Wang <wanghengsheng9@gmail.com>
2026-02-16 10:34:15 -0800

    Finished V3

---
+++ script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+  if (typeof renderMathInElement === "function") {
+    renderMathInElement(document.body, {
+      delimiters: [
+        { left: "$$", right: "$$", display: true },
+        { left: "$", right: "$", display: false }
+      ]
+    });
+  }
+});
